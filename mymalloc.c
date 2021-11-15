c55987b845952ab8d086cc16f8f0acf31d36ca1a
ajgem <79936721+ajgem@users.noreply.github.com>
2021-11-15 16:39:58 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -35,9 +35,10 @@ void myfree(void* ptr){
 }
 
 void* myrealloc(void* ptr, size_t size){
-    if (ptr == NULL && size != 0){
+    if (ptr == NULL ){
         return mymalloc(size);
-    }elseif(ptr != NULL && size == 0){
+    }
+    if(size == 0){
         myfree(ptr);
         return NULL;
 else{

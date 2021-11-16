763df8d199dc4aa645866cd429f5aa2c2d8d82ab
ajgem <79936721+ajgem@users.noreply.github.com>
2021-11-16 13:59:12 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -35,15 +35,19 @@ void myfree(void* ptr){
 }
 
 void* myrealloc(void* ptr, size_t size){
-    if (ptr == NULL ){
+   
+    // checking to see if the pointer is empty if so call mymalloc
+ if (ptr == NULL ){
         return mymalloc(size);
     }
+    // checking size, if zero then call free and return NULL
     if(size == 0){
         myfree(ptr);
         return NULL;
-    }
+}
+    // if neither are attemted then a new space must be created
     void* temp_space = mymalloc(size);
-    memcpy(temp_space, ptr, size);
+    memcpy(temp_space, ptr, size); // using memcopy to use the pointer size and new space
     myfree(ptr);
     ptr = temp_space;
     return ptr;

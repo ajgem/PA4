03a9fa11292b89dfb6ef70faeb04bb7e6b051480
asifh063 <62316164+asifh063@users.noreply.github.com>
2021-11-29 18:52:06 -0500

    added the realloc method from discord

--- malloc.c
+++ malloc.c
@@ -128,6 +128,26 @@ void myfree(void* mem){
 	
 }
 
+void* myrealloc(void* ptr, size_t size){
+
+    // checking to see if the pointer is empty if so call mymalloc
+ if (ptr == NULL ){
+        return mymalloc(size);
+    }
+    // checking size, if zero then call free and return NULL
+    if(size == 0){
+        myfree(ptr);
+        return NULL;
+}
+    // if neither are attemted then a new space must be created
+    void* temp_space = mymalloc(size);
+    memcpy(temp_space, ptr, size); // using memcopy to use the pointer size and new space
+    myfree(ptr);
+    ptr = temp_space;
+    return ptr;
+}
+
+/*
 int main(int argc, char **argv){
 char* test = (char*)malloc(sizeof(char)*4);
 printf("%p\n",(void*)test);
@@ -140,4 +160,4 @@ free(test2);
 char* test4 = (char*)malloc(sizeof(char)*4);
 printf("%p\n",(void*)test4);
 return 0;
-}
+}*/

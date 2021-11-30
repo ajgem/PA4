02717425625c26dd0aa77264e67bd8074e7b5fa6
ajgem <79936721+ajgem@users.noreply.github.com>
2021-11-29 23:31:58 -0500

    Update newMalloc.c

--- newMalloc.c
+++ newMalloc.c
@@ -2,106 +2,91 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <fcntl.h>
+#include <ctype.h>
 #include <errno.h>
+
 // for size and to ensure no memory leaks
 #define MEM_SIZE 5000
-// define headers
-//#define malloc(x) mymalloc(x)
-//#define free(x) myfree(x)
-
+// creating the tempBlock the same size of the mem size 
 static char tempBlock[5000];
 
-/*
-process for malloc:
-	if block is first to be malloced:
-		mem requested > mem available ? err : allocate mem and return pointer to first block after metadata:
-
-	else:
-		get first meta data, check m1 size against m2 address and find how much empty space there is.
-			if the second block and its metadata fits in the empty space, place it there, update next pointers of previous meta data
-			else proceede to next meta data.
-			if metadata.next = null compare size of that meta data with address of last block in array.  
-			if there is room for requested space, return pointer to block, else err
-
-process for free:
-	TBD
-
-
-*/
-typedef struct mymalloc_memory memspot;
-//typedef struct metadata meta;
+typedef struct mymalloc_memory memspot; // using a typedef because it makes delcareing much easier when going thru functions 
 struct mymalloc_memory{
-
  char free_block; // keeping trackof which block is being freed
  int size; // size of the block 
  memspot* nextBlock; // Pointer to lead us to the next slot in memory 
- //0 if used, 1 if free
 }; 
-
+// The goal of this to check the space and size required to malloc
+//if 
 void* mymalloc(int size){
-	//on first malloc
+	//on first malloc findind the amount of space to be used
 	void* storageSpace;
-	if (tempBlock[0]=='\0'){
+    // checking to see the nul character also due to finding null in the first created space
+	if (tempBlock[0] == '\0'){
         // create a holder to see which memory space is being executedd 
 		memspot currentAllocated_space;
         // if  the amount of space is too much than allowed throw an error
+        // if the size wanting to be allocated is greater than te bounds then throw an error 
 		currentAllocated_space.size = size + sizeof(memspot) >= MEM_SIZE ? fprintf(stderr,"Not in bounds\n") : size;
-		currentAllocated_space.free_block = '\?';
-		currentAllocated_space.nextBlock = NULL;
-		//store first metadata struct in address of first block in array
+		currentAllocated_space.free_block = '\?'; //set the freed block to escape format output, in order to format the freed block 
+		currentAllocated_space.nextBlock = NULL; // set equal to Null 
+		//store first memspot struct in address of first space/slot in the array 
 
 		memcpy(&tempBlock[0],&currentAllocated_space,sizeof(memspot));
 
-		//get address of first free block after metadata and return that as void*
+		//get addy of first free block after exectuting memspot and return the storageSpace
 		storageSpace = (void*)(&tempBlock[0] + sizeof(memspot));
 		return storageSpace;
-
+        // just send back the pointer
 	}
 	else {
-		//start with a pointer to first metadata
+		// need to create a temp pointer to be used as the space in line to be placed
 		memspot* temp = (memspot*)&tempBlock[0];
-
+        
 		long placement;
+        // have to check that the something exists
 		while(temp != NULL){
-			//consoladate adjecent free blocks of memory
+			//checking the free space of memory so that the temp block can be placed inside of it 
 			if ((temp -> free_block != '\?') && temp -> nextBlock && (temp -> nextBlock -> free_block != '\?')){
-				temp -> size += temp -> nextBlock-> size +  sizeof(memspot);
-				temp -> nextBlock = temp -> nextBlock -> nextBlock;
+				temp -> size += temp -> nextBlock-> size +  sizeof(memspot); // add on the size onto the next block 
+				temp -> nextBlock = temp -> nextBlock -> nextBlock; // placing the space into the next block 
 			}
-			//if block is already free, no need to create new metadata, just update current one and return pointer. 
-			if ((temp -> free_block != '\?')&&(temp -> size <= size + sizeof(memspot))){
+			//if block is already free, don't create new memspots, j update and return the ptr
+			if ((temp -> free_block != '\?') && (temp -> size <= size + sizeof(memspot))){
 				temp -> size = size;
-				temp -> free_block = '\?';
+				temp -> free_block = '\?'; // formating freed block 
+                // have to convert the temp to a long to ensure the plaecment slot
 				placement = ((long)(temp) - (long)&tempBlock[0]) + sizeof(memspot);
+                // return the space of the temp block 
 				return (void*)(&tempBlock[placement]);
 				}
-
-			memspot* currentMemSpaceEnd = temp -> nextBlock != NULL ? temp -> nextBlock : (memspot*)&tempBlock[MEM_SIZE];
-
+            // creating a slot for the last spot on the array for allocating space 
+            // using t/f statement 
+			memspot* endof_Openspace = temp -> nextBlock != NULL ? temp -> nextBlock : (memspot*)&tempBlock[MEM_SIZE]; // making sure no space of nextblock is empty/NUll
+            // a place holder to see where the temp is going to go depeding on size 
 			placement = ((long)(temp) - (long)&tempBlock[0]) + temp -> size + sizeof(memspot);
 
 			// if block requested fits inbetween currently allocated block and next block, make new metadata and return pointer to allocated block
-			if ((void*)&tempBlock[placement] - (void*)(currentMemSpaceEnd) > (sizeof(memspot) + size)){
+			if ((void*)&tempBlock[placement] - (void*)(endof_Openspace) > (sizeof(memspot) + size)){
 				//insert new pointer 
 				memspot newAllocated_space; // this will be worked on and hold the sizes
 				newAllocated_space.size = size;
-				newAllocated_space.free_block =' \?';
-				newAllocated_space.nextBlock = temp -> nextBlock;
+				newAllocated_space.free_block =' \?'; // same thing as the presoiuv temp 
+				newAllocated_space.nextBlock = temp -> nextBlock; // placing temp into nextb
 
-				//place metadata in array directly after the allocated block
+				//place memspot in array after being allocated
 				memcpy(&tempBlock[placement],&newAllocated_space,sizeof(memspot));
 
 				temp -> nextBlock = (memspot*)&tempBlock[placement];
 				
-				//return pointer to first empty byte after metadata
+				//return pointer to first empty byte after memspot
 				return (void*)(&tempBlock[placement + sizeof(memspot)]);
 			}
-			else {
+			else {// placing temp into other 
 				if (temp -> nextBlock){
 					temp = temp -> nextBlock;
 				}
-				else 
+				else // print that no more space can be added 
 					fprintf(stderr, "out of space\n");
 			}
 		} 
@@ -110,17 +95,16 @@ void* mymalloc(int size){
 }
 
 void myfree(void* ptr){
-	//print error if user is attempting to free a block of memory that is outside the bounds of the simulated "memory"
-	//unsure if the address of mem will actually be withing the range of 0-5000 or if it will have a legitimate address according to actual memory management
-
+	//throw an error if the memory being used it outside the range that was already defined 
+    // also comparing the ptr and making sure it is a certain size 
 	if((char*)ptr > &tempBlock[MEM_SIZE] || (char*)ptr < &tempBlock[0])
 
-	{
+	{   // if not enough spare or exceeding we need to throw an error 
 		fprintf(stderr, "heap overflow\n");
 		return;
 	}
 
-	//make pointer to first byte of metadata corresponding to memory to be freed
+	//make pointer to first space of memspot matching the freed memory 
 	memspot* temp = (memspot*)(ptr - sizeof(memspot));
 	
 	//print error if already free memory is attempting to be be freed
@@ -128,7 +112,7 @@ void myfree(void* ptr){
 	{
 		temp -> free_block = 1;
 	}
-	else
+	else // too much mem would be allocated to overflow error 
 	{
 		fprintf(stderr, "heap overflow\n");
 		return;
@@ -154,18 +138,3 @@ void* myrealloc(void* ptr, size_t size){
     ptr = temp_space;
     return ptr;
 }
-
-/*
-int main(int argc, char **argv){
-char* test = (char*)malloc(sizeof(char)*4);
-printf("%p\n",(void*)test);
-char* test2 = (char*)malloc(sizeof(char)*4);
-printf("%p\n",(void*)test2);
-char* test3 = (char*)malloc(sizeof(char)*4);
-printf("%p\n",(void*)test3);
-
-free(test2);
-char* test4 = (char*)malloc(sizeof(char)*4);
-printf("%p\n",(void*)test4);
-return 0;
-}*/
